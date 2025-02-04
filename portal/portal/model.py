46f46719440c1a93dae50eb5f5aa8c1a4d74db59
Donald Chinhuru <donychinhuru@gmail.com>
2025-02-04 14:08:15 +0200

    db
    initialize db
    initialize tables

--- portal/portal/model.py
+++ portal/portal/model.py
@@ -1,22 +1,53 @@
-from dataclasses import dataclass, field
 from datetime import datetime
-from typing import List
+from typing import List, Optional
+
+import reflex as rx
+from sqlmodel import Field, Relationship
 
 
-@dataclass
-class Message:
-    sender: str
+# @dataclass
+# class Message:
+#     sender: str
+#     content: str
+#     timestamp: str = field(default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+#
+#     def __str__(self):
+#         return f"[{self.timestamp}] {self.sender}: {self.content}"
+#
+# @dataclass
+# class Chat:
+#     sender: str
+#     messages: List[Message] = field(default_factory=list)
+#
+#     def __str__(self):
+#         return f"[{self.sender}] {self.messages}"
+
+
+class Message(rx.Model, table=True):
     content: str
-    timestamp: str = field(default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-    def __str__(self):
-        return f"[{self.timestamp}] {self.sender}: {self.content}"
-
-
-@dataclass
-class Chat:
     sender: str
-    messages: List[Message] = field(default_factory=list)
+    timestamp: datetime = Field(default_factory=datetime.now)
+    chat_id: int = Field(foreign_key="chat.id")
+    chat: Optional["Chat"] = Relationship(
+        back_populates="messages",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
 
-    def __str__(self):
-        return f"[{self.sender}] {self.messages}"
+
+class Chat(rx.Model, table=True):
+    """
+        Main chat model, each user will have a chat entry with conversation messages
+
+        :var sender: client phone number > wa_id
+        :var status: show chat state
+                    1   - requesting live support
+                    0   - acquired communication link
+                   -1   - no agent available
+        :var last_active: last interaction message received
+        :var messages: conversation messages with admin / agent
+
+    """
+    sender: str
+    status: int = -1
+    last_active: datetime = Field(default_factory=datetime.now)
+    messages: List[Message] = Relationship(back_populates="chat")
--- portal/rxconfig.py
+++ portal/rxconfig.py
@@ -2,4 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portal",
+    db_url="sqlite:///pywce_support.db"
 )
